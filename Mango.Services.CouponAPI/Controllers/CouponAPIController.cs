4773f95f9e3a3b4974a064504868db18bc04b8cc
Jordan Baker <Jordan.Baker@likewize.com>
2024-12-27 21:25:15 +0000

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,138 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDTO _response;
+        private IMapper _mapper;
+
+        public CouponAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDTO();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> result = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDTO>>(result);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Coupon result = _db.Coupons.FirstOrDefault(coupon => coupon.CouponId == id);
+                _response.Result = _mapper.Map<Coupon>(result);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDTO GetByCode(string code)
+        {
+            try
+            {
+                Coupon result = _db.Coupons.FirstOrDefault(coupon => coupon.CouponCode.ToLower() == code.ToLower());
+                _response.Result = _mapper.Map<Coupon>(result);
+                _response.IsSuccess = result != null;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDTO Post([FromBody] CouponDTO couponDto)
+        {
+            try
+            {
+                Coupon result = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(result);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(result);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDTO Put([FromBody] CouponDTO couponDto)
+        {
+            try
+            {
+                Coupon result = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(result);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDTO>(result);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDTO Delete(int id)
+        {
+            try
+            {
+                Coupon result = _db.Coupons.FirstOrDefault(coupon => coupon.CouponId == id);
+                _db.Coupons.Remove(result);
+                _db.SaveChanges();
+
+                _response.IsSuccess = result != null;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+    }
+}
---
+++ Mango.Web/Utility/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class StaticDetails
+    {
+        public enum HttpRequestType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
