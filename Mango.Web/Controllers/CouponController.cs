12a5c60391b58a4e6e30a5db0098f2e9c742dd74
Jordan Baker <Jordan.Baker@likewize.com>
2024-12-30 19:45:31 +0000

    Bootstrap theme updated, icons introduced. Coupon service created and made consumable via the coupon controller.

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,31 @@
+﻿using Mango.Web.Models.DTO;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDTO>? list = new();
+
+            ResponseDTO? response = await _couponService.GetAllCouponsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDTO>>(response.Result.ToString());
+            }
+
+            return View(list);
+        }
+    }
+}
---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,76 @@
+﻿using Mango.Web.Models.DTO;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDTO?> CreateCouponAsync(CouponDTO couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                HttpRequest = StaticDetails.HttpRequestType.POST,
+                Data = couponDto,
+                Url = $"{StaticDetails.CouponAPIBase}/api/coupon"
+            });
+        }
+
+        public async Task<ResponseDTO?> DeleteCouponAsync(int couponId)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                HttpRequest = StaticDetails.HttpRequestType.DELETE,
+                Url = $"{StaticDetails.CouponAPIBase}/api/coupon/{couponId}"
+            });
+        }
+
+        public async Task<ResponseDTO?> GetAllCouponByIdAsync(int couponId)
+        {
+            return await _baseService.SendAsync(new RequestDTO() { 
+                HttpRequest = StaticDetails.HttpRequestType.GET, 
+                Url = $"{StaticDetails.CouponAPIBase}/api/coupon/{couponId}" 
+            });
+        }
+
+        public async Task<ResponseDTO?> GetAllCouponsAsync()
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                HttpRequest = StaticDetails.HttpRequestType.GET,
+                Url = $"{StaticDetails.CouponAPIBase}/api/coupon"
+            });
+        }
+
+        public async Task<ResponseDTO?> GetCouponAsync(string couponCode)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                HttpRequest = StaticDetails.HttpRequestType.GET,
+                Url = $"{StaticDetails.CouponAPIBase}/api/coupon/GetByCode/{couponCode}"
+            });
+        }
+
+        public async Task<ResponseDTO?> UpdateCouponAsync(CouponDTO couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                HttpRequest = StaticDetails.HttpRequestType.PUT,
+                Data = couponDto,
+                Url = $"{StaticDetails.CouponAPIBase}/api/coupon"
+            });
+        }
+
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+    }
+}
