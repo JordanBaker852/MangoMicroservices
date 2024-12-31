23336a5d10fb4e85917038334a0e23d4740f37bd
Jordan Baker <Jordan.Baker@likewize.com>
2024-12-31 08:02:22 +0000

    Coupon views started to help perform CRUD operations on the coupon service.

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,8 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Mango.Web.Controllers
 {
@@ -14,7 +16,7 @@ namespace Mango.Web.Controllers
             _couponService = couponService;
         }
 
-        public async Task<IActionResult> CouponIndex()
+        public async Task<IActionResult> Index()
         {
             List<CouponDTO>? list = new();
 
@@ -26,6 +28,54 @@ namespace Mango.Web.Controllers
             }
 
             return View(list);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CouponDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDTO? response = await _couponService.CreateCouponAsync(model);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Delete(int couponId)
+        {
+            ResponseDTO? response = await _couponService.GetAllCouponByIdAsync(couponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                CouponDTO? model = JsonConvert.DeserializeObject<CouponDTO>(response.Result.ToString());
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCoupon(int couponId)
+        {
+            ResponseDTO? response = await _couponService.DeleteCouponAsync(couponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
         }
     }
 }
