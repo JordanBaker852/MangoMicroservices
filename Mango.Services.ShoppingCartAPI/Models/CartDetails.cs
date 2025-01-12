adac6f35e25449d63d290ceb912fb2ca6fa964a5
Jordan Baker <Jordan.Baker@likewize.com>
2025-01-12 16:48:37 +0000

    Shopping Cart API project startup configuration and shopping cart tables added to the DB.

---
+++ Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; } = new();
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDTO Product { get; set; } = new();
+        public int Quantity { get; set; }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Models/DTO/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.DTO
+{
+    public class CouponDTO
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
