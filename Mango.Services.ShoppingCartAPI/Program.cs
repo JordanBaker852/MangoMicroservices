2c7b47075ac3aea82fb784f1f8738ca6454f5db4
Jordan Baker <Jordan.Baker@likewize.com>
2025-02-11 19:34:32 +0000

    Access token passed into product and coupon HttpClient for shopping cart authorisation

--- Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -4,6 +4,7 @@ using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Extentions;
 using Mango.Services.ShoppingCartAPI.Service;
 using Mango.Services.ShoppingCartAPI.Service.IService;
+using Mango.Services.ShoppingCartAPI.Utility;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -19,10 +20,21 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<BackendApiAuthenticationHttpClientHandler>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
-builder.Services.AddHttpClient("Product", client => client.BaseAddress = new Uri(builder.Configuration.GetSection("ServiceUrls:ProductAPI").Get<string>()));
-builder.Services.AddHttpClient("Coupon", client => client.BaseAddress = new Uri(builder.Configuration.GetSection("ServiceUrls:CouponAPI").Get<string>()));
+
+builder.Services.AddHttpClient("Product", client => client.BaseAddress = 
+    new Uri(builder.Configuration.GetSection("ServiceUrls:ProductAPI").Get<string>())
+).AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
+
+builder.Services.AddHttpClient("Coupon", client => client.BaseAddress = 
+    new Uri(builder.Configuration.GetSection("ServiceUrls:CouponAPI").Get<string>())
+).AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
