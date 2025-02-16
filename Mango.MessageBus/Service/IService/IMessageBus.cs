70df8f668030d6da725d9c40561b071f44d65704
Jordan Baker <Jordan.Baker@likewize.com>
2025-02-16 16:22:19 +0000

    Message bus service created using Azure Service Bus
    The service bus coinnection string will be passed throgh the constructor to prevent git security from blocking the commit push.

---
+++ Mango.MessageBus/Service/IService/IMessageBus.cs
@@ -0,0 +1,7 @@
+﻿namespace Mango.MessageBus.Service.IService
+{
+    public interface IMessageBus : IDisposable
+    {
+        public Task PublishMessage(object message, string topicQueueName);
+    }
+}
---
+++ Mango.MessageBus/Service/MessageBus.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Messaging.ServiceBus;
+using Mango.MessageBus.Service.IService;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mango.MessageBus.Service
+{
+    public class MessageBus : IMessageBus
+    {
+        private readonly string _connectionString;
+
+        public MessageBus(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task PublishMessage(object message, string topicQueueName)
+        {
+            await using var client = new ServiceBusClient(_connectionString, new ServiceBusClientOptions()
+            {
+                TransportType = ServiceBusTransportType.AmqpWebSockets
+            });
+
+            ServiceBusSender sender = client.CreateSender(topicQueueName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            var finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage)) 
+            { 
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+            
+            await sender.SendMessageAsync(finalMessage);
+
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
